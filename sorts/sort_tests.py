7dbbc7391d0a9256108c450624496af89841194a
evie <evie.adams.2000@gmail.com>
2025-07-11 14:27:38 -0700

    add selection sort implemention and basic unit tests

---
+++ sorts/sort_tests.py
@@ -0,0 +1,58 @@
+# sorting algorithm unit tests
+
+import unittest
+import sorts
+
+sorting_algorithms = {
+    "bubblesort" : sorts.bubble_sort,
+    "selectionsort" : sorts.selection_sort
+}
+
+class TestSortAlgorithms(unittest.TestCase):
+    def test_nop(self):
+        presorted_list_ascending = [-23, -4, 0, 0, 1, 4, 100, 1000003]
+        self.assertListEqual(presorted_list_ascending, sorted(presorted_list_ascending))
+
+        presorted_list_descending = [234234, 3888, 100, 3, 3, 0, -13944]
+        self.assertListEqual(presorted_list_descending, sorted(presorted_list_descending, reverse = True))
+
+        for func in sorting_algorithms.values():
+            list_copy = presorted_list_ascending.copy()
+            func(list_copy)
+            self.assertListEqual(presorted_list_ascending, list_copy)
+
+            list_copy = presorted_list_descending.copy()
+            func(list_copy, sort_order=sorts.SortOrder.DESCENDING)
+            self.assertListEqual(presorted_list_descending, list_copy)
+    
+    def test_empty_single(self):
+        for test_list in [[], [-387]]:
+            for func in sorting_algorithms.values():
+                list_copy = test_list.copy()
+                func(list_copy)
+                self.assertListEqual(test_list, list_copy)
+
+                list_copy = test_list.copy()
+                func(list_copy, sort_order=sorts.SortOrder.DESCENDING)
+                self.assertListEqual(test_list, list_copy)
+
+    def test_sorting(self):
+        for test_list in [[-967153, 180291, 250110, -842723, -3701, -612791, 43993, -181828, 51011, -932719],
+                           [81497, 836583, 536516, 894021, -744399, -943861, -900306, -679152, 591792, 748742],
+                           [-202762, 212425, 458334, -522110, -16120, -902054, 194856, -705583, -975078, -231770],
+                           [537882, 106327, -969394, -970028, -628415, -469152, 874998, 602016, -13642, 142053, -672324, 176585, 524556, 411173, 226438, -814207, -758328, -269396, 758683, -346637, 982485, 336673, -789908, 336895, 719382, -982120, 281233, 767464, 121961, 895145, 784854, 607146, 470868, -175230, -602783, 721081, 563912, -973268, -713882, -638066, -237577, 884272, 974477, 523431, -660390, 750220, 419840, -293960, 684515, 716476, 30490, -468599, 941430, 966037, 765302, -840941, -41393, 730398, 305591, 292857, 174911, -238487, 644796, 490125, -920638, -850486, 121758, 427218, -538489, 486189, -147310, -915622, 455439, 994182, 759865, 296754, -198477, 906701, 695123, -138270, -554559, 945902, 194742, 870587, 390195, -314450, -733717, 572109, 331323, 519645, -548263, 393727, -613340, -102897, -281751, 245079, -776500, -759912, 793850, 296949],
+                           [-476606, -521133, 408305],
+                           ['abc', 'zdf', 'a', 'aaaa', '0x45']]:
+            for func in sorting_algorithms.values():
+                list_copy = test_list.copy()
+                list_sorted = sorted(test_list)
+                func(list_copy)
+                self.assertListEqual(list_copy, list_sorted)
+
+                list_copy = test_list.copy()
+                list_sorted = sorted(test_list, reverse = True)
+                func(list_copy, sort_order=sorts.SortOrder.DESCENDING)
+                self.assertListEqual(list_copy, list_sorted)
+
+if __name__ == '__main__':
+    unittest.main()
--- sorts/sorts.py
+++ sorts/sorts.py
@@ -6,12 +6,20 @@ class SortOrder(Enum):
     ASCENDING = 0
     DESCENDING = 1
 
+def default_comparator(a, b):
+    if a < b:
+        return -1
+    elif a > b:
+        return 1
+    else:
+        return 0
+
 def swap(list, index0, index1):
     swap = list[index0]
     list[index0] = list[index1]
     list[index1] = swap
 
-def bubble_sort(list, comparator, sort_order = SortOrder.ASCENDING):
+def bubble_sort(list, comparator = default_comparator, sort_order = SortOrder.ASCENDING):
     if sort_order not in SortOrder:
         raise ValueError("sort_order must be in " + list(SortOrder))
 
@@ -19,8 +27,28 @@ def bubble_sort(list, comparator, sort_order = SortOrder.ASCENDING):
         swap_performed = False
         for j in range(len(list) - 1):
             comparison = comparator(list[j], list[j + 1])
-            if (sort_order == SortOrder.ASCENDING and comparison > 0) or (sort_order == SortOrder.DESCENDING and comparison < 0):
+            if sort_order == SortOrder.DESCENDING:
+                comparison *= -1
+            if comparison > 0:
                 swap(list, j, j + 1)
                 swap_performed = True
         if not swap_performed:
             return
+        
+def selection_sort(list, comparator = default_comparator, sort_order = SortOrder.ASCENDING):
+    if sort_order not in SortOrder:
+        raise ValueError("sort_order must be in " + list(SortOrder))
+    
+    for i in range(len(list)):
+        lowest_index = i
+        lowest_value = list[i]
+        for j in range(i + 1, len(list)):
+            comparison = comparator(list[j], lowest_value)
+            if sort_order == SortOrder.DESCENDING:
+                comparison *= -1
+            if comparison < 0:
+                lowest_index = j
+                lowest_value = list[j]
+        if lowest_index != i:
+            swap(list, i, lowest_index)
+
